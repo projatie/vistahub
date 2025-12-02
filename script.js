4f6022dced86d85e28c274de788ba6fbdabf8e28
GreanGuyGG <111597285+GreanGuyGG@users.noreply.github.com>
2025-12-02 11:06:53 +0100

    ok

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const cookiebox = document.getElementById("cookiebox")
-const cookiebutton = document.getElementsByClassName("cookiebutton")
+const cookiebox = document.getElementById("cookiebox");
+const cookiebutton = document.getElementsByClassName("cookiebutton");
 
 function show(){
     cookiebox.style.display = "none"
