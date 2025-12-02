756607518e66d465e89b873bb7d9c3db6b6c1e9e
GreanGuyGG <111597285+GreanGuyGG@users.noreply.github.com>
2025-12-02 11:16:30 +0100

    ok

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const cookiebox = document.getElementById("cookiebox");
 const cookiebutton = document.getElementsByClassName("cookiebutton");
 
-function show(){
-    cookiebox.style.display = "none"
-};
+//function show(){
+    //cookiebox.style.display = "none"
+//}
 
 console.log("test")
