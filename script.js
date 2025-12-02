a7b8172dc0a12c9b92ce7228393ead2c0f31b87b
GreanGuyGG <111597285+GreanGuyGG@users.noreply.github.com>
2025-12-02 09:50:07 +0100

    change

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout (function)() {
+    document.getElementById ('cookiebox').classList.remove ('hidden')
+}, 3000);
+function acceptCookies(){document.getElementById ('cookiebox').style.display = 'none';
+
+} 
